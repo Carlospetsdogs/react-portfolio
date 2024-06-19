6a7b39b7f0f7794b80af4435fda8c8345e05bd1e
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-06-19 08:26:31 -0400

    added aboutme.jsx and an assets folder with a portrait photo

---
+++ src/components/aboutme.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <img src="./assets/portraits/Developer-photo.jpeg" alt="Developer" />
+      <p>I am a very ametuerish developer with a lot of ambition and creativity.</p>
+    </section>
+  );
+};
+
+export default AboutMe;
