7122ba5af7b769a55e5cdff01231dfa065fa98d4
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-06-20 07:59:33 -0400

    added resume pdf to resume.jsx

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import resume from "../assets/CarlosRuiz.pdf"
 
 const Resume = () => {
   const proficiencies = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js'];
@@ -6,7 +7,7 @@ const Resume = () => {
   return (
     <section>
       <h2>Resume</h2>
-      <a href="path-to-your-resume.pdf" download>Download Resume</a>
+      <a href={resume} download>Download Resume</a>
       <h3>Proficiencies</h3>
       <ul>
         {proficiencies.map((skill, index) => (
